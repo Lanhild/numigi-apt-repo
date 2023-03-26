3189295fd08bac68bce6b8cb3c93956a7f879e02
Lanhild <joliveau.loan@gmail.com>
2023-03-25 20:35:44 -0400

    numigi-hooks TO 1.0.1

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # numigi-apt-repo
-// Repository used to host custom hooks and packages
+// Repository used to host custom hooks and packages used in [Numigi](https://numigi.com) laptops.
 
 ## Produce a release
-1. Place all your packages into the main pool
+1. Place all your packages into the main pool, named accordingly to the conventions of the repository
 
-2. Run the build script
+2. Run the build script in the current directory
 ```bash
 python3 gen-release.py
 ```
 
 3. Commit your changes
 
-Your package repository should be ready.
+Your package repository should be ready after building.
--- gen-release.py
+++ gen-release.py
@@ -4,73 +4,89 @@
 # The Release file is signed with the GPG key of the repository.
 
 import os
-import sys
 
-# Get the current date.
-current_date = os.popen("date -Ru").read().strip()
+# Generate a new release
+def generate_release():
+    # Verify if the KEY.asc file is present. If not, list the user's gpg secret keys and ask them to choose one.
+    # Then, export the chosen key to the KEY.asc file.
+    def verify_key_file():
+        if not os.path.isfile("KEY.asc"):
+            print("No GPG key found. Please choose one from the list below:")
+            print("--------------------------------------------------")
+            os.system("gpg --list-secret-keys --keyid-format=long")
+            print("--------------------------------------------------")
+            key_id = input("Enter the ID of the key you want to use: ")
+            os.system(f"gpg --armor --export {key_id} > KEY.asc")
+            print("--------------------------------------------------")
+            print("Public GPG key exported to KEY.asc file.")
+            print("--------------------------------------------------")
 
-### Clean up ###
-# Delete the Packages, Packages.gz, Release, Release.gpg and InRelease files if they exist.
-def delete_existing_files():
-    print("Deleting old build files...")
-    print("--------------------------------------------------")
-    os.system("rm -f dists/stable/main/binary-amd64/Packages")
-    os.system("rm -f dists/stable/main/binary-amd64/Packages.gz")
-    os.system("rm -f dists/stable/Release")
-    os.system("rm -f dists/stable/Release.gpg")
-    os.system("rm -f dists/stable/InRelease")
+    verify_key_file()
 
-delete_existing_files()
+    # Delete old release files.
+    def delete_existing_files():
+        print("Deleting old build files...")
+        print("--------------------------------------------------")
+        os.system("rm -f dists/stable/main/binary-amd64/Packages")
+        os.system("rm -f dists/stable/main/binary-amd64/Packages.gz")
+        os.system("rm -f dists/stable/Release")
+        os.system("rm -f dists/stable/Release.gpg")
+        os.system("rm -f dists/stable/InRelease")
 
-### Packages file generation ###
-# Function to generate the Packages file.
-def generate_packages_file():
-    print("Generating Packages file...")
-    print("--------------------------------------------------")
-    os.system("dpkg-scanpackages --multiversion pool/ > dists/stable/main/binary-amd64/Packages")
-    # Zip the Packages file.
-    os.system("gzip -k -f dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.gz")
+    delete_existing_files()
 
-generate_packages_file()
+    ### Generate release files ###
+    def generate_release_files():
+        # Packages file generation.
+        def packages_file():
+            print("Generating Packages file...")
+            print("--------------------------------------------------")
+            os.system("dpkg-scanpackages --multiversion pool/ > dists/stable/main/binary-amd64/Packages")
+            # Zip the Packages file.
+            os.system("gzip -k -f dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.gz")
 
-### Release file generation ###
-# Append repo information to the Release file.
-def append_repo_info_to_release_file():
-    print("Appending repo information to Release file...")
-    print("--------------------------------------------------")
-    with open("dists/stable/Release", "a") as release_file:
-        release_file.write("Origin: Numigi repository\n")
-        release_file.write("Label: numigi-apt-repo\n")
-        release_file.write("Suite: stable\n")
-        release_file.write("Codename: stable\n")
-        release_file.write("Version: 1.0\n")
-        release_file.write("Architectures: amd64 arm64 arm7\n")
-        release_file.write("Components: main\n")
-        release_file.write("Description: Numigi custom packages repository\n")
-        release_file.write(f"Date: {current_date}\n")
+        packages_file()
 
-append_repo_info_to_release_file()
+        # Append repo information to the Release file.
+        def release_file():
+            print("Appending repo information to Release file...")
+            print("--------------------------------------------------")
+            with open("dists/stable/Release", "a") as release_file:
+                release_file.write("Origin: Numigi repository\n")
+                release_file.write("Label: numigi-apt-repo\n")
+                release_file.write("Suite: stable\n")
+                release_file.write("Codename: stable\n")
+                release_file.write("Version: 1.0\n")
+                release_file.write("Architectures: amd64 arm64 arm7\n")
+                release_file.write("Components: main\n")
+                release_file.write("Description: Numigi custom packages repository\n")
+                
+        release_file()
+        
+        # Append checksums to the Release file.
+        def release_checksums():
+            print("Appending checksums to Release file...")
+            print("--------------------------------------------------")
+            os.system("apt-ftparchive release dists/stable >> dists/stable/Release")
+            
+        release_checksums()
+        
+        # Sign the Release file.
+        def sign_release():
+            print("Signing Release file...")
+            print("--------------------------------------------------")
+            os.system('gpg --default-key "joliveau.loan@gmail.com" -abs -o - dists/stable/Release > dists/stable/Release.gpg')
+            
+        sign_release()
+        
+        # Create the InRelease file.
+        def add_in_release():
+            print("Creating InRelease file...")
+            print("--------------------------------------------------")
+            os.system('gpg --default-key "joliveau.loan@gmail.com" --clearsign -o - dists/stable/Release > dists/stable/InRelease')
+            
+        add_in_release()
+    
+    generate_release_files()
 
-# Append checksums to the Release file.
-def append_checksums_to_release_file():
-    print("Appending checksums to Release file...")
-    print("--------------------------------------------------")
-    os.system("apt-ftparchive release dists/stable >> dists/stable/Release")
-
-append_checksums_to_release_file()
-
-# Function to sign the Release file.
-def sign_release_file():
-    print("Signing Release file...")
-    print("--------------------------------------------------")
-    os.system("gpg --default-key \"joliveau.loan@gmail.com\" -abs -o - dists/stable/Release > dists/stable/Release.gpg")
-
-sign_release_file()
-
-# Function to create the InRelease file.
-def create_in_release_file():
-    print("Creating InRelease file...")
-    print("--------------------------------------------------")
-    os.system("gpg --default-key \"joliveau.loan@gmail.com\" --clearsign -o - dists/stable/Release > dists/stable/InRelease")
-
-create_in_release_file()
+generate_release()
